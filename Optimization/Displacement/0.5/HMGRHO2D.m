ac6b3aede9af19d91315e12fe11fa2325a21f2f5
qmdxcube <67142522+qmdxcube@users.noreply.github.com>
2025-12-09 13:15:26 +0800

    Delete Optimization/Displacement/0.5 directory

--- Optimization/Displacement/0.5/HMGRHO2D.m
+++
@@ -1,4 +0,0 @@
-function [rhoH,drhoH]=HMGRHO2D(xmic,rho)
-xr=xmic(:);
-rhoH=rho*mean(xr);
-drhoH=rho/length(xr);
--- Optimization/Displacement/0.5/MacEMI.m
+++
@@ -1,5 +0,0 @@
-function [xp,dxp]=MacEMI(x)
-penal=3;alpha=15/16;
-xp=alpha*x.^penal+(1-alpha)*x;
-dxp=alpha*penal*x.^(penal-1)+(1-alpha);
-end
