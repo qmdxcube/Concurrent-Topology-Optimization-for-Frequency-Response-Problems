a8d6a373ad73c8dd52e8078bf8982bb8bac411e5
qmdxcube <67142522+qmdxcube@users.noreply.github.com>
2025-12-09 13:46:26 +0800

    Delete Concurrent Topology Optimization/Acceleration/codes directory

--- Concurrent Topology Optimization/Acceleration/codes/MicEMI.m
+++
@@ -1,5 +0,0 @@
-function [xp,dxp]=MicEMI(x)
-penal=3;
-xp=x.^penal;
-dxp=penal*x.^(penal-1);
-end
--- Concurrent Topology Optimization/Acceleration/codes/qxw.m
+++
@@ -1,7 +0,0 @@
-function [gxx,gww]= qxw(sOmega,eOmega,num_Omega)
-%% This code only can be used for the case of sOmega=0; for other cases it should be modified
-dOmega=(eOmega-sOmega)/num_Omega;
-gxx=sOmega+(0:num_Omega-1)'*dOmega;
-gww=dOmega*ones(num_Omega,1);
-gww(1)=dOmega/2;gww(end)=dOmega/2;
-end
